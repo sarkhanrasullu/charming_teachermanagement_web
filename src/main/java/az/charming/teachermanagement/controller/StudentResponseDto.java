8ffda57c3d27d5089ab8186c1c827e8b6eb7440d
sarkhanr <sarkhan.rasullu@bankrespublika.az>
2021-06-11 13:27:56 +0400

    changes

---
+++ src/main/java/az/charming/teachermanagement/controller/StudentResponseDto.java
@@ -0,0 +1,57 @@
+package az.charming.teachermanagement.controller.dto.response;
+
+import java.math.BigDecimal;
+
+public class StudentResponseDto {
+
+    private Integer id;
+    private String name;
+    private String surname;
+    private Integer age;
+    private BigDecimal scholarship;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public StudentResponseDto setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public StudentResponseDto setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public StudentResponseDto setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public StudentResponseDto setAge(Integer age) {
+        this.age = age;
+        return this;
+    }
+
+    public BigDecimal getScholarship() {
+        return scholarship;
+    }
+
+    public StudentResponseDto setScholarship(BigDecimal scholarship) {
+        this.scholarship = scholarship;
+        return this;
+    }
+}
