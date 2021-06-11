871d17159d32b8888e2ba26ce1b6be3a479f274f
sarkhanr <sarkhan.rasullu@bankrespublika.az>
2021-06-11 13:38:22 +0400

    changes

--- src/main/java/az/charming/teachermanagement/controller/MainController.java
+++ src/main/java/az/charming/teachermanagement/controller/MainController.java
@@ -1,19 +1,4 @@
-/*
- * Copyright 2002-2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package sample.web;
+package az.charming.teachermanagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -41,6 +26,7 @@ public class MainController {
 	@Autowired
 	private OAuth2AuthorizedClientService authorizedClientService;
 
+
 	@GetMapping("/")
 	public String index(Model model, OAuth2AuthenticationToken authentication) {
 		OAuth2AuthorizedClient authorizedClient =
@@ -60,16 +46,16 @@ public class MainController {
 						authentication.getName());
 		Map userAttributes = Collections.emptyMap();
 		String userInfoEndpointUri = authorizedClient.getClientRegistration()
-			.getProviderDetails().getUserInfoEndpoint().getUri();
+				.getProviderDetails().getUserInfoEndpoint().getUri();
 		if (!StringUtils.isEmpty(userInfoEndpointUri)) {	// userInfoEndpointUri is optional for OIDC Clients
 			userAttributes = WebClient.builder()
-				.filter(oauth2Credentials(authorizedClient))
-				.build()
-				.get()
-				.uri(userInfoEndpointUri)
-				.retrieve()
-				.bodyToMono(Map.class)
-				.block();
+					.filter(oauth2Credentials(authorizedClient))
+					.build()
+					.get()
+					.uri(userInfoEndpointUri)
+					.retrieve()
+					.bodyToMono(Map.class)
+					.block();
 		}
 		model.addAttribute("userAttributes", userAttributes);
 		return "userinfo";
@@ -77,11 +63,12 @@ public class MainController {
 
 	private ExchangeFilterFunction oauth2Credentials(OAuth2AuthorizedClient authorizedClient) {
 		return ExchangeFilterFunction.ofRequestProcessor(
-			clientRequest -> {
-				ClientRequest authorizedRequest = ClientRequest.from(clientRequest)
-					.header(HttpHeaders.AUTHORIZATION, "Bearer " + authorizedClient.getAccessToken().getTokenValue())
-					.build();
-				return Mono.just(authorizedRequest);
-			});
+				clientRequest -> {
+					ClientRequest authorizedRequest = ClientRequest.from(clientRequest)
+							.header(HttpHeaders.AUTHORIZATION, "Bearer " + authorizedClient.getAccessToken().getTokenValue())
+							.build();
+					return Mono.just(authorizedRequest);
+				});
 	}
+
 }
